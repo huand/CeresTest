8f7c3dc5d3509db29bd2d2979e51d9f2f4ff46ca
Hubert Andre <hubert.andre.1991@gmail.com>
2019-07-31 20:02:00 +0200

    redo ok

--- helloworld.cc
+++ helloworld.cc
@@ -1,29 +1,26 @@
 #include <iostream>
 #include "ceres/ceres.h"
-
+#include "glog/logging.h"
 struct CostFunctor {
   template <typename T>
-  bool operator()(const T* const x, T* residual) const {
-    residual[0] = T(10.0) - x[0];
+  bool operator()(const T *const x, T *res) const {
+    res[0] = static_cast<T>(10) - x[0];
     return true;
   }
 };
 
-double MyFun(double x) { return x * 10; }
-
-int main(int argc, char const *argv[])
-{
-  double x = 12221.123;
-  ceres::Problem problem;
-  ceres::CostFunction* cost_function =
-  new ceres::AutoDiffCostFunction<CostFunctor, 1, 1>(new CostFunctor);
-  problem.AddResidualBlock(cost_function, NULL, &x);
-
+int main(int argc, char const *argv[]) {
   ceres::Solver::Options options;
-  options.linear_solver_type = ceres::DENSE_QR;
   options.minimizer_progress_to_stdout = true;
+  options.linear_solver_type = ceres::DENSE_QR;
+  ceres::Problem problem;
+  ceres::CostFunction *cost_function =
+      new ceres::AutoDiffCostFunction<CostFunctor, 1, 1>(new CostFunctor);
+  double x0 = 100;
+  double x = x0;
+  problem.AddResidualBlock(cost_function, nullptr, &x);
   ceres::Solver::Summary summary;
   ceres::Solve(options, &problem, &summary);
-  std::cout << summary.BriefReport() << std::endl;
+  std::cout << summary.FullReport() << std::endl;
   return 0;
 }
--- powell.cc
+++ powell.cc
@@ -1,82 +1,67 @@
-#include <vector>
+#include <iostream>
 #include "ceres/ceres.h"
-#include "gflags/gflags.h"
 #include "glog/logging.h"
 
-using ceres::AutoDiffCostFunction;
-using ceres::CostFunction;
-using ceres::Problem;
-using ceres::Solve;
-using ceres::Solver;
-
-struct F1 {
+struct CostFunctor1 {
   template <typename T>
   bool operator()(const T* const x1, const T* const x2, T* residual) const {
-    // f1 = x1 + 10 * x2;
-    residual[0] = x1[0] + 10.0 * x2[0];
+    residual[0] = x1[0] + (T)10 * x2[0];
     return true;
   }
 };
-struct F2 {
+struct CostFunctor2 {
   template <typename T>
   bool operator()(const T* const x3, const T* const x4, T* residual) const {
-    // f2 = sqrt(5) (x3 - x4)
-    residual[0] = sqrt(5.0) * (x3[0] - x4[0]);
+    residual[0] = sqrt(5) * (x3[0] - x4[0]);
     return true;
   }
 };
-struct F3 {
+struct CostFunctor3 {
   template <typename T>
   bool operator()(const T* const x2, const T* const x3, T* residual) const {
-    // f3 = (x2 - 2 x3)^2
-    residual[0] = (x2[0] - 2.0 * x3[0]) * (x2[0] - 2.0 * x3[0]);
+    residual[0] = pow(x2[0] - (T)2 * x3[0], 2);
     return true;
   }
 };
-struct F4 {
+struct CostFunctor4 {
   template <typename T>
   bool operator()(const T* const x1, const T* const x4, T* residual) const {
-    // f4 = sqrt(10) (x1 - x4)^2
-    residual[0] = sqrt(10.0) * (x1[0] - x4[0]) * (x1[0] - x4[0]);
+    residual[0] = sqrt(10) * pow(x1[0] - x4[0], 2);
     return true;
   }
 };
-DEFINE_string(minimizer, "trust_region",
-              "Minimizer type to use, choices are: line_search & trust_region");
-int main(int argc, char** argv) {
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
-  google::InitGoogleLogging(argv[0]);
-  double x1 = 3.0;
-  double x2 = -1.0;
-  double x3 = 0.0;
-  double x4 = 1.0;
-  Problem problem;
-  // Add residual terms to the problem using the using the autodiff
-  // wrapper to get the derivatives automatically. The parameters, x1 through
-  // x4, are modified in place.
-  problem.AddResidualBlock(new AutoDiffCostFunction<F1, 1, 1, 1>(new F1), NULL,
-                           &x1, &x2);
-  problem.AddResidualBlock(new AutoDiffCostFunction<F2, 1, 1, 1>(new F2), NULL,
-                           &x3, &x4);
-  problem.AddResidualBlock(new AutoDiffCostFunction<F3, 1, 1, 1>(new F3), NULL,
-                           &x2, &x3);
-  problem.AddResidualBlock(new AutoDiffCostFunction<F4, 1, 1, 1>(new F4), NULL,
-                           &x1, &x4);
-  Solver::Options options;
-  LOG_IF(FATAL, !ceres::StringToMinimizerType(FLAGS_minimizer,
-                                              &options.minimizer_type))
-      << "Invalid minimizer: " << FLAGS_minimizer
-      << ", valid options are: trust_region and line_search.";
-  options.max_num_iterations = 100;
-  options.linear_solver_type = ceres::DENSE_QR;
+
+int main(int argc, char const* argv[]) {
+  ceres::Problem problem;
+  ceres::CostFunction* costfunction1 =
+      new ceres::AutoDiffCostFunction<CostFunctor1, 1, 1, 1>(new CostFunctor1);
+  ceres::CostFunction* costfunction2 =
+      new ceres::AutoDiffCostFunction<CostFunctor2, 1, 1, 1>(new CostFunctor2);
+  ceres::CostFunction* costfunction3 =
+      new ceres::AutoDiffCostFunction<CostFunctor3, 1, 1, 1>(new CostFunctor3);
+  ceres::CostFunction* costfunction4 =
+      new ceres::AutoDiffCostFunction<CostFunctor4, 1, 1, 1>(new CostFunctor4);
+  double x1 = 3;
+  double x2 = -1;
+  double x3 = 0;
+  double x4 = 1;
+  problem.AddResidualBlock(costfunction1, nullptr,
+                           std::vector<double*>{&x1, &x2});
+  problem.AddResidualBlock(costfunction2, nullptr,
+                           std::vector<double*>{&x3, &x4});
+  problem.AddResidualBlock(costfunction3, nullptr,
+                           std::vector<double*>{&x2, &x3});
+  problem.AddResidualBlock(costfunction4, nullptr,
+                           std::vector<double*>{&x1, &x4});
+  ceres::Solver::Options options;
+  ceres::Solver::Summary summary;
   options.minimizer_progress_to_stdout = true;
-  std::cout << "Initial x1 = " << x1 << ", x2 = " << x2 << ", x3 = " << x3
-            << ", x4 = " << x4 << "\n";
-  // Run the solver!
-  Solver::Summary summary;
-  Solve(options, &problem, &summary);
-  std::cout << summary.FullReport() << "\n";
-  std::cout << "Final x1 = " << x1 << ", x2 = " << x2 << ", x3 = " << x3
-            << ", x4 = " << x4 << "\n";
+  ceres::Solve(options, &problem, &summary);
+  std::cout << "x1: " << x1 << '\n';
+  std::cout << "x2: " << x2 << '\n';
+  std::cout << "x3: " << x3 << '\n';
+  std::cout << "x4: " << x4 << '\n';
+  std::cout << summary.FullReport() << std::endl;
+  std::cout << summary.IsSolutionUsable() << '\n';
   return 0;
 }
