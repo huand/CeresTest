0d102adb32fc6fc67203500167f402f3b4cdd91f
Hubert Andre <hubert.andre.1991@gmail.com>
2019-08-04 12:38:03 +0200

    circle ok

--- ceres/circle.cc
+++ ceres/circle.cc
@@ -27,10 +27,20 @@ struct CircleData {
 struct Distancor {
   template <typename T>
   bool operator()(const T* const radius, const T* const center,
-                  const T* rodrigues, T* res) {
-    double u[] = {1, 0, 0};
-    double n[3];
-    ceres::AngleAxisRotatePoint(rodrigues, u, n);
+                  const T* const rodrigues, T* res) const {
+    T u[] = {(T)0., (T)0., (T)1.};
+    T n[3];
+    ceres::AngleAxisRotatePoint((T*)rodrigues, u, n);
+    T cross[3];
+    T xc[] = {xyz[0] - center[0], xyz[1] - center[1], xyz[2] - center[2]};
+    ceres::CrossProduct(n, xc, cross);
+    ceres::CrossProduct(cross, n, u);
+    T length = sqrt(ceres::DotProduct(u, u));
+    T v[] = {radius[0] * u[0] / length, radius[0] * u[1] / length,
+             radius[0] * u[2] / length};
+    res[0] = xc[0] - v[0];
+    res[1] = xc[1] - v[1];
+    res[2] = xc[2] - v[2];
     return true;
   }
   double xyz[3];
@@ -38,6 +48,30 @@ struct Distancor {
 
 int main(int argc, char const* argv[]) {
   CircleData circle("data/circle_noisy.csv");
-
+  ceres::Solver::Options options;
+  ceres::Solver::Summary summary;
+  ceres::Problem problem;
+  double radius = 20;
+  double center[] = {0, 0, 0};
+  double rodrigues[] = {0, 0, 0};
+  /////////////////
+  Distancor dor{20, 0, 0};
+  double res[3];
+  dor(&radius, center, rodrigues, res);
+  /////////////////
+  for (int i = 0; i < circle.points_.size(); i++) {
+    ceres::CostFunction* cost_function =
+        new ceres::AutoDiffCostFunction<Distancor, 3, 1, 3, 3>(
+            new Distancor{{circle.points_[i].xyz[0], circle.points_[i].xyz[1],
+                           circle.points_[i].xyz[2]}});
+    problem.AddResidualBlock(cost_function, nullptr, &radius, center,
+                             rodrigues);
+  }
+  ceres::Solve(options, &problem, &summary);
+  std::cout << summary.FullReport();
+  std::cout << "r: " << radius << '\n';
+  std::cout << "x: " << center[0] << '\n';
+  std::cout << "y: " << center[1] << '\n';
+  std::cout << "z: " << center[2] << '\n';
   return 0;
 }
--- python/make_circle.py
+++ python/make_circle.py
@@ -13,18 +13,18 @@ data = r*np.concatenate((np.cos(theta), np.sin(theta),
 circle = r*np.concatenate((np.cos(theta0), np.sin(theta0),
                            np.zeros((n, 1))), axis=1)
 # adding noise
-noise_level = r
+noise_level = r/7
 data += np.random.randn(n, 3) * noise_level
 
 # solid 3d transformation
-theta = np.random.rand() * 2 * np.pi
-phi = np.random.rand() * 2 * np.pi
+theta = np.random.rand() * np.pi*2
+phi = np.random.rand() * 2 * np.pi*2
 Rtheta = np.array(((np.cos(theta), 0, np.sin(theta)),
                    (0, 1, 0), (-np.sin(theta), 0, np.cos(theta))))
 Rphi = np.array(
     ((1, 0, 0), (0, np.cos(phi), np.sin(phi)), (0, -np.sin(phi), np.cos(phi))))
 R = Rtheta.dot(Rphi)
-t = np.random.rand(3, 1) * r
+t = np.random.rand(3, 1) * r/2
 T01 = np.concatenate(
     (np.concatenate((R, np.zeros((1, 3)))), np.concatenate((t, np.ones((1, 1))))), axis=1)
 circle = np.concatenate((circle, np.ones((n, 1))), axis=1)
@@ -39,7 +39,7 @@ ax.axis = 'equal'
 size = np.ones(n)
 ax.scatter(data[:, 0], data[:, 1], data[:, 2], s=size)
 ax.plot(circle[:, 0], circle[:, 1], circle[:, 2], 'r')
-np.savetxt("../data/circle_noisy.csv", data, delimiter=" ")
-np.savetxt("../data/circle_pure.csv", circle, delimiter=" ")
+np.savetxt("data/circle_noisy.csv", data, delimiter=" ")
+np.savetxt("data/circle_pure.csv", circle, delimiter=" ")
 ax.axis = 'equal'
 plt.show()
